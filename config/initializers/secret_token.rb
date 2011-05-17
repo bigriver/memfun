168ce1a86ea29d00968fe1f36bdb8b093a22e5d3
Peter O'Braidan <pspetroff@gmail.com>
2011-05-17 11:10:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memfun::Application.config.secret_token = '54fa499fff7470e3ef63eff957aa607af5f4c3839293b5c623ec8ee58b989a662fc20ad0fe04cc44eed4690efcf5ac0448626b41e9ab413476a7f3af0d20ad70'
